27c032334daa4de674769fe4e541e8366d66ee22
unknown <mariaservicetest@gmail.com>
2021-06-10 12:14:55 -0500

    solucion exercice2

--- DevTraining2021/src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ DevTraining2021/src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -1,8 +1,30 @@
 package com.sngular.devTraining2021.exercises;
 
+
 public class AlgorithmName {
-    public static String accum(String str) {
-        // your code
-    	return "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu";
-    }
+	public static String accum(String str) {
+		// your code
+
+		String result = "";
+		// i es la 
+		for (int i = 0; i < str.length(); i++) {
+			char caracter = str.charAt(i);
+			
+			for (int j = 0; j <= i; j++) {
+				if (j == 0) {
+					result += Character.toUpperCase(caracter);
+				} else {
+					result += Character.toLowerCase(caracter);
+				}
+			}
+			result += "-";
+		}
+		
+		//replazamos el utimo carcter de la cadena - despues de remplazar el char
+		String re= result.replaceFirst(".$","");
+		System.out.println(re);
+		
+		return re;
+
+	}
 }
