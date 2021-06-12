da94762bfb0f8a91f9a37994a5d89c7d0af3e274
unknown <mariaservicetest@gmail.com>
2021-06-12 12:56:22 -0500

    solucion del ejercicio 5

--- DevTraining2021/src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
+++ DevTraining2021/src/main/java/com/sngular/devTraining2021/exercises/AlgorithmName.java
@@ -1,8 +1,21 @@
 package com.sngular.devTraining2021.exercises;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AlgorithmName {
     public static int[] minMax(int[] arr) {
         // Your awesome code here
-    	return arr;
+    	
+    	ArrayList<Integer> array = new ArrayList<>();
+    	for(int a : arr ) {
+    		
+    		array.add(a);
+    	}
+    	int max = Collections.max(array);
+    	int min = Collections.min(array);
+    	return new int [] {min,max};
+    	
     }
+    
 }
